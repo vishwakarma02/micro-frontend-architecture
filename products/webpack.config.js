14fa47d3226fc25c105b50b5c0b2156b02f7bfbc
Vishwakarma02 <vijay.arpit79@gmail.com>
2023-12-23 22:15:19 +0530

    sharing dependencies between apps

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
             exposes: {
                 './ProductsIndex': './src/index',
             },
+            shared: ['faker'],
         }),
         new HtmlWebpackPlugin({
             template: './public/index.html'
